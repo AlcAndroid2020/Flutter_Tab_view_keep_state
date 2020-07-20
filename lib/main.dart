a7d1f42a296994fed80d2f2a1c1ad0b6d1ce954d
cstanALC <cstan.alc@gmail.com>
2020-07-20 16:49:04 +0800

    added add post button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Bottom navigator bar'),
@@ -24,26 +23,16 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin,AutomaticKeepAliveClientMixin {
-  int _counter = 0,_currentindex=0;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
+  int _currentindex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
   PageController pageController = PageController(
     initialPage: 0,
     keepPage: true,
@@ -51,73 +40,92 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
-      body:  PageView(
+      body: PageView(
         controller: pageController,
-
-        children: <Widget>[
-         HomeTab(),
-          MenuTab(),
-      ProfileTab(),
-        ],
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-
-      bottomNavigationBar: BottomNavigationBar(
-
-        currentIndex: _currentindex,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home),title:Text("Home")),
-          BottomNavigationBarItem(icon: Icon(Icons.menu),title:Text("Menu")),
-          BottomNavigationBarItem(icon: Icon(Icons.person),title:Text("Profile")),
-
-
-        ],
-        onTap: (index){
+        onPageChanged: (page){
           setState(() {
-            _currentindex=index;
-            pageController.animateToPage(index, duration: Duration(milliseconds: 500), curve: Curves.ease);
+            _currentindex = page;
 
           });
         },
-      ),// This trailing comma makes auto-formatting nicer for build methods.
+        children: <Widget>[
+          HomeTab(),
+          MenuTab(),
+          ProfileTab(),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentindex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), title: Text("Menu")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), title: Text("Profile")),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentindex = index;
+
+          });
+        },
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
   @override
   // TODO: implement wantKeepAlive
   bool get wantKeepAlive => true;
-
 }
+
 class HomeTab extends StatefulWidget {
   @override
   _HomeTabState createState() => _HomeTabState();
 }
 
 class _HomeTabState extends State<HomeTab> with AutomaticKeepAliveClientMixin {
-  bool isClicked=false;
+  bool isClicked = false;
+  List<String> PostList = ["post1"];
+
+  @override
+  void initState() {
+    print("Home");
+    super.initState();
+  }
+
+  renderPost(String post, int index) {
+    return Container(
+      height: 100,
+      color: Colors.green,
+      child: Text(
+        post,
+        style: TextStyle(fontSize: 20, color: Colors.white),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        isClicked?  SizedBox(height: 250,):Container(),
-        Center(child: Container(child: Text("Home"),)),
-        RaisedButton(onPressed:(){
-          setState(() {
-            isClicked=true;
-          });
-        } ,child: Text("Move to middle"))
+        RaisedButton(
+            onPressed: () {
+              setState(() {
+                isClicked = true;
+                PostList.add("Post ${PostList.length + 1}");
+              });
+            },
+            child: Text("add Post")),
+        Flexible(
+          child: ListView.builder(
+            itemCount: PostList.length,
+            itemBuilder: (context, i) => renderPost(PostList[i], i),
+          ),
+        ),
       ],
     );
   }
@@ -126,54 +134,97 @@ class _HomeTabState extends State<HomeTab> with AutomaticKeepAliveClientMixin {
   // TODO: implement wantKeepAlive
   bool get wantKeepAlive => true;
 }
+
 class MenuTab extends StatefulWidget {
   @override
   _MenuTabState createState() => _MenuTabState();
 }
 
-class _MenuTabState extends State<MenuTab> with AutomaticKeepAliveClientMixin{
-  bool isClicked=false;
+class _MenuTabState extends State<MenuTab> with AutomaticKeepAliveClientMixin {
+  bool isClicked = false;
+  List<String> toDoList = ["todo1"];
+  @override
+  void initState() {
+    print("Menu");
+    super.initState();
+  }
+  renderTodos(String todos, int index) {
+    return Container(
+      height: 100,
+      color: Colors.blue,
+      child: Text(
+        todos,
+        style: TextStyle(fontSize: 20, color: Colors.white),
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        isClicked?  SizedBox(height: 250,):Container(),
-        Center(child: Container(child: Text("Menu"),)),
-        RaisedButton(onPressed:(){
-          setState(() {
-            isClicked=true;
-          });
-        }
-        ,child: Text("Move to middle"),)      ],
+        RaisedButton(
+            onPressed: () {
+              setState(() {
+                isClicked = true;
+                toDoList.add("Post ${toDoList.length + 1}");
+              });
+            },
+            child: Text("add Post")),
+        Flexible(
+          child: ListView.builder(
+            itemCount: toDoList.length,
+            itemBuilder: (context, i) => renderTodos(toDoList[i], i),
+          ),
+        ),
+      ],
     );
   }
 
   @override
   // TODO: implement wantKeepAlive
-  bool get wantKeepAlive =>true;
+  bool get wantKeepAlive => true;
 }
+
 class ProfileTab extends StatefulWidget {
   @override
   _ProfileTabState createState() => _ProfileTabState();
 }
 
-class _ProfileTabState extends State<ProfileTab> with AutomaticKeepAliveClientMixin {
-  bool isClicked=false;
+class _ProfileTabState extends State<ProfileTab>
+    with AutomaticKeepAliveClientMixin {
+  bool isClicked = false;
+
+  @override
+  void initState() {
+    print("Profile");
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-
-      isClicked?  SizedBox(height: 250,):Container(),
-        Center(child: Container(child: Text("Profile"),)),
-      RaisedButton(onPressed:(){
-        setState(() {
-          isClicked=true;
-        });
-      } ,child: Text("Move to middle"))
+        isClicked
+            ? SizedBox(
+                height: 250,
+              )
+            : Container(),
+        Center(
+            child: Container(
+          child: Text("Profile"),
+        )),
+        RaisedButton(
+            onPressed: () {
+              setState(() {
+                isClicked = true;
+              });
+            },
+            child: Text("Move to middle"))
       ],
-    );  }
+    );
+  }
 
   @override
   // TODO: implement wantKeepAlive
